7fa428b6603c7052db4649035ab1e37d041fe44a
AmineAhn <amineahniche@gmail.com>
2017-11-08 18:58:36 +0000

    Committing in order to move to ActivityUserProfile development

---
+++ app/src/main/java/com/example/amine/exporttofirebase/ActivityUserProfile.java
@@ -0,0 +1,13 @@
+package com.example.amine.exporttofirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
